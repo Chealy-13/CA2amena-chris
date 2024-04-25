6038ef68c10f34e68d000bc5e9c9117db008204b
Chealy-13 <chris.healy33@gmail.com>
2024-04-26 00:16:15 +0100

    Added test for get()
    tesig to make sure returns null on getting from empty map or if entry does not exist and testing on an entry that doees exist

--- src/test/java/HashMapTest.java
+++ src/test/java/HashMapTest.java
@@ -36,4 +36,25 @@ class HashMapTest {
         //double check value was updted
         assertEquals(updatedPatient, map.get(key1));
     }
+
+    @Test
+    void get() {
+        HashMap map = new HashMap();
+        LocalDate dob1 = LocalDate.of(1990, 5, 15);
+        PatientId key1 = new PatientId("John", "Doe", dob1);
+        Patient patient1 = new Patient("John", "Doe", dob1, LocalDate.now());
+
+        //testing on empty map
+        assertEquals(null, map.get(key1));
+
+        //testing on an existing entry
+        map.put(key1, patient1);
+        assertEquals(patient1, map.get(key1));
+
+        //testing if key is wrong/does not exist
+        LocalDate dob2 = LocalDate.of(1985, 8, 20);
+        PatientId key2 = new PatientId("Billy", "joel", dob2);
+        assertEquals(null, map.get(key2));
+
+    }
 }
